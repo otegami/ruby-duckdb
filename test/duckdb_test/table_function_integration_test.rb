8b1d2f04e472f85e879b640100b27e03781f2a45
otegami <a.s.takuya1026@gmail.com>
2026-04-02 19:58:42 +0900

    feat: remove SET threads=1 requirement for table functions on DuckDB >= 1.5.0

--- test/duckdb_test/table_function_integration_test.rb
+++ test/duckdb_test/table_function_integration_test.rb
@@ -7,7 +7,9 @@ module DuckDBTest
     def setup
       @database = DuckDB::Database.open
       @connection = @database.connect
-      @connection.execute('SET threads=1') # Required for Ruby callbacks
+      return unless Gem::Version.new(DuckDB::LIBRARY_VERSION) < Gem::Version.new('1.5.0')
+
+      @connection.execute('SET threads=1')
     end
 
     def teardown
